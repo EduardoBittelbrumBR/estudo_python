b6858a8f8a24d5557dc084cca20e7dd229cf2dcb
EduardoBittelbrumBR <edubittelbrum@gmail.com>
2023-06-30 21:41:34 -0300

    Syntax comando input, entrada de informações em tela

---
+++ input.py
@@ -0,0 +1,12 @@
+nome=input('Qual seu nome? ')
+idade=input('Digitar idade: ')
+peso=input('Digitar peso: ')
+nota1=input('Digite nota 1: ')
+nota2=input('Digite nota 2: ')
+soma=0
+soma=nota1+nota2
+
+print('Nome ', nome,
+      'Idade ', idade,
+      'Peso ', peso)
+print('A soma da nota é: ', soma)
