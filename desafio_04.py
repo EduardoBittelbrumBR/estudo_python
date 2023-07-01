df242a587107a0ba6fbb2c02e485ffef94dc7c79
EduardoBittelbrumBR <edubittelbrum@gmail.com>
2023-06-30 22:36:41 -0300

    Aula sobre tipos primitivos e desafio 04, testa usando funções IS.

---
+++ desafio_04.py
@@ -0,0 +1,6 @@
+entrada = input('Digite algo: ')
+
+print('Entrada é número? {}'.format(entrada.isnumeric()))
+print('Entrada possui letras? {}'.format(entrada.isalpha()))
+print('Entrada possui letras maiúsculas? {}'.format(entrada.isupper()))
+print('Entrada possui é alpha numérico? {}'.format(entrada.isalpha()))
---
+++ tipos_primitivos.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digita primeiro valor: '))
+n2 = int(input('Digita segundo valor: '))
+
+soma = n1+n2
+print('A soma é: {}'.format(soma))
