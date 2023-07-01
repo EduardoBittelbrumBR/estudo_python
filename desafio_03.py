e20105e53340c672e28d9dcd6117cd42633555c2
EduardoBittelbrumBR <edubittelbrum@gmail.com>
2023-06-30 21:40:02 -0300

    Desafio 3, soma dois números e mostra em tela

---
+++ desafio_03.py
@@ -0,0 +1,13 @@
+print('---Desafio 03---');
+
+print('ENTRADAS');
+num1=int(input('Digite o primeiro número: '));
+num2=int(input('Digite o segundo número: '));
+
+print('SOMA');
+#declara variável soma
+soma=0
+soma=num1+num2
+
+#printa em tela o valor da soma
+print('O valor da soma é: ', soma);
