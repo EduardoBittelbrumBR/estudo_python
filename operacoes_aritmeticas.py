d159533abfbcc25c5114248885bf8e3ac556ad78
EduardoBittelbrumBR <edubittelbrum@gmail.com>
2023-07-01 11:40:49 -0300

    Aula sobre operações aritmeticas.

---
+++ operacoes_aritmeticas.py
@@ -0,0 +1,46 @@
+# variáveis primeiro cálculo
+
+num1 = 5
+num2 = 3
+num3 = 2
+
+# calcula e mostra em tela
+
+result = num1 + num2 * num3
+print('O primeiro resultado é: {}'.format(result))
+
+# variáveis segundo cálculo
+
+num4 = 3
+num5 = 5
+num6 = 4
+num7 = 2
+
+# calcula e mostra em tela
+
+result2 = num4 * num5 + num6 ** num7
+print('O segundo resultado é: {}'.format(result2))
+
+# variáveis terceiro cálculo
+
+num8 = 3
+num9 = 5
+num10 = 4
+num11 = 2
+
+# calcula e mostra em tela
+
+result3 = num8 * (num9 + num10) ** num11
+print('O terceiro resultado é: {}'.format(result3))
+
+# a função abaixo executa um cálculo elevando um número ao cubo
+
+cubo1 = pow(4, 3)
+
+print('Mostra em tela valor de ao cubo usando função pow: {}'.format(cubo1))
+
+# o cálculo abaixo executa 4 ao cubo usando operador aritimético
+
+cubo2 = 4**3
+
+print('Mostra em tela valor de ao cubo usando operador: {}'.format(cubo2))
---
+++ operacoes_aritmeticas2.py
@@ -0,0 +1,19 @@
+# entrada de valores
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+# calculos
+
+s = n1 + n2
+sub = n1 - n2
+div = n1 / n2
+mult = n1 * n2
+di = n1 // n2
+pot = n1 ** n2
+
+print('Valor soma: {}'.format(s))
+print('Valor subtração: {}'.format(sub))
+print('Valor divisão: {}'.format(div))
+print('Valor multiplicação: {}'.format(mult))
+print('Valor divisão inteira: {}'.format(di))
+print('Valor potência: {}'.format(pot))
