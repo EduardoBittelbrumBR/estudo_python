21334f2581a2c74489c0b8ff5702a2f0394d5695
EduardoBittelbrumBR <edubittelbrum@gmail.com>
2023-07-03 19:44:00 -0300

    Exercicio corrigido

--- desafio_18.py
+++ desafio_18.py
@@ -1,11 +1,11 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno,
 # Cosseno e tangente
 
-from math import cos, sin, tan
+from math import cos, sin, tan, radians
 
 a = float(input('Digite o valor do ângulo: '))
 
 print('Para o ângulo {} temos:'
       '\nCosseno: {:.2f}'
       '\nSeno: {:.2f}'
-      '\n Tangente: {:.2f}'.format(a, cos(a), sin(a), tan(a)))
+      '\nTangente: {:.2f}'.format(a, cos(radians(a)), sin(radians(a)), tan(radians(a))))
